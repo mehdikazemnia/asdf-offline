823275c8b7aeb9c686dd9c0b7f2f512906523979
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2019-06-23 21:43:15 +0430

    tip-tap 101

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,17 @@
 <template>
     <main>
         <LeftSection/>
+        <CenterSection/>
     </main>
 </template>
 
 <script>
 import LeftSection from '@/components/LeftSection.vue'
+import CenterSection from '@/components/CenterSection.vue'
 export default {
     components: {
-        LeftSection
+        LeftSection,
+        CenterSection
     },
     created() {
         // this.$store.state.category.categories = []
@@ -31,6 +34,11 @@ export default {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+}
+main {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
 }
 </style>
 
