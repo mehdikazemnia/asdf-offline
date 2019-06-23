c8f96c2e8dc1b6a481338b8f372787ab8b670380
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2019-06-23 15:59:16 +0430

    simple edits

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,6 +9,17 @@ import LeftSection from '@/components/LeftSection.vue'
 export default {
     components: {
         LeftSection
+    },
+    created() {
+        // this.$store.state.category.categories = []
+        // make the root if not already there
+        // this code will run only the first time
+        if (
+            !this.$store.state.category.categories.filter(c => c._id == 'c0')
+                .length
+        ) {
+            this.$store.commit('category/CREATE_ROOT')
+        }
     }
 }
 </script>
